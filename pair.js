4be679f852f8b90184aa71a2353b7730d4d765ac
kingkezzu <kingkezzu@gmail.com>
2026-03-03 16:26:32 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -142,7 +142,7 @@ const checkApiKey = async () => {
 // ---------------- MONGO SETUP ----------------
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://camalkaakash2_db_user:QVIRCgDpbjr2adcb@dtznovaxpaspapers.ddt0qup.mongodb.net/?appName=dtznovaxpaspapers';
-const MONGO_DB = process.env.MONGO_DB || 'BESTIE-MD';
+const MONGO_DB = process.env.MONGO_DB || 'BESTIE-MD1';
 
 let mongoClient, mongoDB;
 let sessionsCol, numbersCol, adminsCol, newsletterCol, configsCol, newsletterReactsCol, followChannelsCol;
